0c2571f99292bdce2b8ec75c361e7ea5f724236c
Victor Tsay <vt0826@hotmail.com>
2019-04-16 00:32:28 -0400

    import css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import IndexContainer from "../components/indexContainer"
+import "../components/layout.css"
+
 const IndexPage = () => (
   <div>
     <IndexContainer />
